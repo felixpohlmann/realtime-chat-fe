86b598e003dfa50d9315e290279b47ebc52f758d
Felix Pohlmann <felixpohlmann.dev@gmail.com>
2021-02-18 18:21:05 +0100

    basic layout

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="app">
+      <div className="sidebar"></div>
+      <div className="main"></div>
+    </div>
+  );
+};
+
+export default App;
