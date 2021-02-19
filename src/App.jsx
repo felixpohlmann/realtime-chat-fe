5f7c564e57dd8e1388e3094f89b1c465d78d98f4
Felix Pohlmann <felixpohlmann.dev@gmail.com>
2021-02-19 15:37:43 +0100

    added contact layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+
+//components
+import Sidebar from "./components/Sidebar/Sidebar";
+
+//css
 import "./App.css";
 
 const App = () => {
   return (
     <div className="app">
-      <div className="sidebar"></div>
-      <div className="main"></div>
+      <div className="app__sidebar">
+        <Sidebar />
+      </div>
+      <div className="app__main"></div>
     </div>
   );
 };
---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Sidebar.css";
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <ul className="contacts">
+        <li className="contact"></li>
+        <li className="contact"></li>
+        <li className="contact"></li>
+        <li className="contact"></li>
+        <li className="contact"></li>
+        <li className="contact"></li>
+        <li className="contact"></li>
+        <li className="contact"></li>
+        <li className="contact"></li>
+        <li className="contact"></li>
+        <li className="contact"></li>
+        <li className="contact"></li>
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
+
+/* Sketch doesnt export pattern fills at this point */
